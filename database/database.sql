70d6650a787f2dc52afcf8a3bc5a1842fd29835b
Bao Duy <bpoduy2@gmail.com>
2026-03-16 12:23:47 +0700

    temp changes

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 07, 2026 lúc 04:29 AM
+-- Thời gian đã tạo: Th3 16, 2026 lúc 06:19 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -43,7 +43,7 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `name`, `phone`, `email`, `password`, `address`, `created_at`, `updated_at`) VALUES
-(1, 'customer1', '0934567890', 'customer1@local.com', '$2y$10$J5lyUr5D3qzG/0OZvSnByuh6B4BWtew1WdCiuIyfuEinZ.z8QtZpa', 'Phan Boi Chau, Thong Nhat, Dong Nai', '2026-03-07 02:42:55', '2026-03-07 02:42:55'),
+(1, 'customer1', '0934567890', 'customer1@gmail.com', '$2y$10$J5lyUr5D3qzG/0OZvSnByuh6B4BWtew1WdCiuIyfuEinZ.z8QtZpa', 'Phan Boi Chau, Thong Nhat, Dong Nai', '2026-03-07 02:42:55', '2026-03-16 03:40:38'),
 (2, 'Lê Đình Bảo Duy', '', 'bpoduy2@gmail.com', '$2y$10$KaLJ4rdsyiar9lbd42LnVeaioeQhBI51kmTDbIyoFU10abkBVZUla', '', '2026-03-07 03:16:39', '2026-03-07 03:16:39');
 
 -- --------------------------------------------------------
@@ -164,9 +164,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
-(4, 'Admin', 'admin@local.com', '$2y$10$3vVTcxrKExF622yp9HJ.9O0rKlAJSTX2Kgfy1eUr10gsjtqg74LUK', 'admin', '2026-02-05 14:08:52', '2026-02-05 14:08:52'),
-(5, 'Manager Test', 'manager@local.com', '$2y$10$3dixkaUpFZeQk5ICAjqbEebnf6Mc8zi9hT8NzRSIVEqq4cSblViHi', 'manager', '2026-02-05 18:53:04', '2026-02-05 18:53:04'),
-(6, 'Staff Test', 'staff@local.com', '$2y$10$aNKcSalp7qdb/OZYuhhaCOOwfWPpCnhoF/ub9Zq8F9M5mLyz8pEw2', 'staff', '2026-02-05 18:53:05', '2026-02-05 18:53:05');
+(4, 'Admin', 'admin@gmail.com', '$2y$10$3vVTcxrKExF622yp9HJ.9O0rKlAJSTX2Kgfy1eUr10gsjtqg74LUK', 'admin', '2026-02-05 14:08:52', '2026-03-16 03:41:09'),
+(5, 'Manager Test', 'manager@gmail.com', '$2y$10$3dixkaUpFZeQk5ICAjqbEebnf6Mc8zi9hT8NzRSIVEqq4cSblViHi', 'manager', '2026-02-05 18:53:04', '2026-03-16 03:41:44'),
+(6, 'Staff Test', 'staff@gmail.com', '$2y$10$aNKcSalp7qdb/OZYuhhaCOOwfWPpCnhoF/ub9Zq8F9M5mLyz8pEw2', 'staff', '2026-02-05 18:53:05', '2026-03-16 03:42:10'),
+(8, 'Test', 'Test@gmail.com', '$2y$10$Q0fIXjdl3aVbza1yWmkwz.RksGeS54dMkX7v0qicrj/0QjQ322izS', 'manager', '2026-03-15 15:36:02', '2026-03-15 15:36:02');
 
 -- --------------------------------------------------------
 
@@ -263,7 +264,7 @@ ALTER TABLE `warranty_extensions`
 -- AUTO_INCREMENT cho bảng `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT cho bảng `devices`
@@ -305,7 +306,7 @@ ALTER TABLE `repair_tickets`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `warranty_extensions`
